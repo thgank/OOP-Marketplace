581ddaa14056b1bcff7c6d79d9538a24f0d936af
thgank <122772347+thgank@users.noreply.github.com>
2023-02-02 15:54:29 +0600

    Add files via upload

---
+++ as3 marketplace/src/devices/Device.java
@@ -0,0 +1,43 @@
+package devices;
+
+public abstract class Device implements DeviceSpecification {
+    private static int idCounter = 0;
+    private String brand;
+    private String model;
+    private double price;
+    private int id;
+
+    public Device(String brand, String model, double price) {
+        setBrand(brand);
+        setModel(model);
+        setPrice(price);
+        setId(++idCounter);
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    protected void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ as3 marketplace/src/devices/gadgets/Smartwatch.java
@@ -0,0 +1,40 @@
+package devices.gadgets;
+
+public class Smartwatch  extends Gadget{
+    private String operatingSystem;
+    private boolean fitnessTracker;
+    private boolean heartRate;
+
+    public Smartwatch(String brand, String model, double price, String type, String operatingSystem, boolean fitnessTracker, boolean heartRate) {
+        super(brand, model, price, type);
+        setOperatingSystem(operatingSystem);
+        setFitnessTracker(fitnessTracker);
+        setHeartRate(heartRate);
+    }
+
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setFitnessTracker(boolean fitnessTracker) {
+        this.fitnessTracker = fitnessTracker;
+    }
+
+    public void setHeartRate(boolean heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public boolean isFitnessTracker() {
+        return fitnessTracker;
+    }
+
+    public boolean isHeartRate() {
+        return heartRate;
+    }
+
+}
