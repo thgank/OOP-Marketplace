b56ce5c30860c750f36e7946d97c8cf1c9678a13
thgank <122772347+thgank@users.noreply.github.com>
2023-04-24 23:45:11 +0600

    Delete as3 marketplace directory

--- as3 marketplace/src/devices/factory/interfaces/GFactory.java
+++
@@ -1,8 +0,0 @@
-package devices.factory.interfaces;
-
-import devices.gadgets.*;
-
-public interface GFactory {
-    Headphones createHeadphones(String brand, String model, double price, String type, String audioQuality, boolean noiseCancel);
-    Smartwatch createSmartwatch(String brand, String model, double price, String type, String operatingSystem, boolean fitnessTracker, boolean heartRate);
-}
--- as3 marketplace/src/devices/factory/interfaces/TFactory.java
+++
@@ -1,8 +0,0 @@
-package devices.factory.interfaces;
-import devices.tablets.*;
-
-public interface TFactory {
-    BusinessTablet createBusinessTablet(String brand, String model, double price, boolean stylusSup, boolean keyboardSup, String screenRatio, boolean fingerprintScanner, boolean facialRecognition);
-    DrawingTablet createDrawingTablet(String brand, String model, double price, boolean stylusSup, boolean keyboardSup, String screenRatio, int pressureSensitivity, boolean eraser);
-    GamingTablet createGamingTablet(String brand, String model, double price, boolean stylusSup, boolean keyboardSup, String screenRatio, int memory, int ram, String gpu, boolean vrSupport);
-}
